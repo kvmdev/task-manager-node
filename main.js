fbdf8cab11cfe209f12fbe1fe485c7a650373052
Kevin Vazquez <kv937398@gmail.com>
2024-01-05 15:36:26 -0800

    The auth endpoint was added

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const usersRouter = require('./routes/users');
 
 app.use(express.json());
 
-app.use('/users', usersRouter);
+app.use("/", require("./routes/auth"));
+app.use("/users", require('./routes/users'));
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
