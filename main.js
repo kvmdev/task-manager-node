54bcca31b5ca4922fc16a2bb27ea97dffd770b18
Kevin Vazquez <kv937398@gmail.com>
2024-01-06 09:59:43 -0800

    Added the pages router to the server

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+const path = require("path");
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use("/api/users", require("./routes/users"));
+app.use("/", require("./routes/pages"));
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
