fa6325120aa0303af53da3c009a8ff2c7d886ada
Kevin Vazquez <kv937398@gmail.com>
2024-01-11 07:40:37 -0800

    Added public static folder in the server

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
