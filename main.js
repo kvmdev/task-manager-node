f11f0d8305e36a67424664ce88c3ecbd69820db6
Kevin Vazquez <kv937398@gmail.com>
2024-01-11 04:32:50 -0800

    Setted views engine and setted the location of the pages files

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", require("./routes/pages"));
 app.use("/api/users", require("./routes/users"));
 
 app.listen(3000, () => {
