0303e58b50567f2c4609d0e0d609897ce83f2072
Kevin Vazquez <kv937398@gmail.com>
2024-01-11 07:39:11 -0800

    The route which gets id by params deleted

--- routes/pages.js
+++ routes/pages.js
@@ -4,7 +4,6 @@ const { PagesController } = require("../controllers/PagesController");
 router.get("/", PagesController.getAllUsers);
 router.get("/update", PagesController.updateUserPage);
 router.get("/create", PagesController.createUserPage);
-router.get("/delete", PagesController.deleteUserPage);
-router.get("/:id", PagesController.getUserById);    
+router.get("/delete", PagesController.deleteUserPage); 
 
 module.exports = router;
