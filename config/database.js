0b3320bb5cadc7d1185898db8393b2787ac093d5
Kevin Vazquez <kv937398@gmail.com>
2024-01-22 05:22:25 -0800

    Changed db password and improved client part

--- config/database.js
+++ config/database.js
@@ -3,6 +3,6 @@ const mysql = require('mysql2/promise');
 module.exports = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "6294862",
     database: "task_manager_db"
 });
