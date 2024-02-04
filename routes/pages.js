8f9cf97c89369c9bccfb4e164bd6dba2a061d38e
Kevin Vazquez <kv937398@gmail.com>
2024-02-04 11:59:25 -0800

    Deleted the delete page

--- routes/pages.js
+++ routes/pages.js
@@ -4,7 +4,6 @@ const { PagesController } = require("../controllers/PagesController");
 router.get("/", PagesController.getAllTasks);
 router.get("/update", PagesController.updateUserPage);
 router.get("/create", PagesController.createUserPage);
-router.get("/delete", PagesController.deleteUserPage); 
 router.get("/login", PagesController.loginPage);
 router.get("/logout", PagesController.logout);
 router.get("/register", PagesController.registerPage);
